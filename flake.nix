dd94a913ed66069ecee50e747d8581c275af7521
rolfst <rolfst@gmail.com>
2023-10-13 09:46:12 +0200

    feat(template): add typescript template

--- flake.nix
+++ flake.nix
@@ -3,63 +3,62 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      overlays = [
-        (final: prev:
-          let
-            exec = pkg: "${prev.${pkg}}/bin/${pkg}";
-          in
-          {
-            format = prev.writeScriptBin "format" ''
-              ${exec "nixpkgs-fmt"} **/*.nix
-            '';
-            dvt = prev.writeScriptBin "dvt" ''
-              if [ -z $1 ]; then
-                echo "no template specified"
-                exit 1
-              fi
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    overlays = [
+      (final: prev: let
+        exec = pkg: "${prev.${pkg}}/bin/${pkg}";
+      in {
+        format = prev.writeScriptBin "format" ''
+          ${exec "nixpkgs-fmt"} **/*.nix
+        '';
+        dvt = prev.writeScriptBin "dvt" ''
+          if [ -z $1 ]; then
+            echo "no template specified"
+            exit 1
+          fi
 
-              TEMPLATE=$1
+          TEMPLATE=$1
 
-              ${exec "nix"} \
-                --experimental-features 'nix-command flakes' \
-                flake init \
-                --template \
-                "github:the-nix-way/dev-templates#''${TEMPLATE}"
-            '';
-            update = prev.writeScriptBin "update" ''
-              for dir in `ls -d */`; do # Iterate through all the templates
-                (
-                  cd $dir
-                  ${exec "nix"} flake update # Update flake.lock
-                  ${exec "nix"} flake check  # Make sure things work after the update
-                )
-              done
-            '';
-          })
-      ];
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-    in
+          ${exec "nix"} \
+            --experimental-features 'nix-command flakes' \
+            flake init \
+            --template \
+            "github:the-nix-way/dev-templates#''${TEMPLATE}"
+        '';
+        update = prev.writeScriptBin "update" ''
+          for dir in `ls -d */`; do # Iterate through all the templates
+            (
+              cd $dir
+              ${exec "nix"} flake update # Update flake.lock
+              ${exec "nix"} flake check  # Make sure things work after the update
+            )
+          done
+        '';
+      })
+    ];
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit overlays system;};
+        });
+  in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
+      devShells = forEachSupportedSystem ({pkgs}: {
         default = pkgs.mkShell {
-          packages = with pkgs; [ format update ];
+          packages = with pkgs; [format update];
         };
       });
 
-      packages = forEachSupportedSystem ({ pkgs }: rec {
+      packages = forEachSupportedSystem ({pkgs}: rec {
         default = dvt;
         inherit (pkgs) dvt;
       });
     }
-
-    //
-
-    {
+    // {
       templates = rec {
         clojure = {
           path = ./clojure;
@@ -199,6 +198,11 @@
         shell = {
           path = ./shell;
           description = "Shell script development environment";
+        };
+
+        typescript = {
+          path = ./typescript;
+          description = "Typescript development environment";
         };
 
         zig = {
