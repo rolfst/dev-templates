20b6b660b3c930e98d1f63320d7f95d8222291c2
rolfst <rolfst@gmail.com>
2023-12-08 13:15:34 +0100

    chore(typescript): bump to nodejs 20

--- typescript/flake.nix
+++ typescript/flake.nix
@@ -9,7 +9,7 @@
   }: let
     overlays = [
       (final: prev: rec {
-        nodejs = prev.nodejs-18_x;
+        nodejs = prev.nodejs_20;
         pnpm = prev.nodePackages.pnpm;
         yarn = prev.yarn.override {inherit nodejs;};
       })
